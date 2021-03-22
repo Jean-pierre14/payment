0c4b6f12f18732a3507d872247f36c8a4579d00e
User <chiruzabisimwa@gmail.com>
2021-03-22 14:25:58 +0200

    list-group-item-action

--- configuration/action.php
+++ configuration/action.php
@@ -7,10 +7,10 @@ if(isset($_POST['action'])){
     if($_POST['action'] == 'allStudents'){
         $sql = mysqli_query($con, "SELECT * FROM student ORDER BY id_student DESC");
         if(@mysqli_num_rows($sql) > 0){
-            $output .= '<ul lass="list-group">';
+            $output .= '<ul lass="list-group shadow-sm">';
             while($row = mysqli_fetch_assoc($sql)){
                 $output .= '
-                    <li class="list-group-item">
+                    <li class="list-group-item list-group-item-action">
                         <a href="?student='.$row['id_student'].'">'.$row['sname'].'</a>
                     </li>
                 ';
