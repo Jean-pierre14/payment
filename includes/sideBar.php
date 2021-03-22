990e99df3ed467ef09610bc8a376f8d68adc4b35
User <chiruzabisimwa@gmail.com>
2021-03-22 11:53:43 +0200

    Marks function

--- includes/sideBar.php
+++ includes/sideBar.php
@@ -16,6 +16,7 @@
         <li class="d-flex justify-content-between align-items-center"><a href="student.php"><i class="icon graduation"></i><label>Students<span class="badge bg-light black"><?php print $mount_Student; ?></span></label></a></li>
         <li class="d-flex justify-content-between align-items-center"><a href="lecturer.php"><i class="icon users"></i><label for="">Lecturers<span class="badge bg-light black"><?php print $mount_Lecturer; ?></span></label></a></li>
         <!-- <li class="d-flex justify-content-between align-items-center"><a href=""><i class="icon trash"></i><label for="">Employees<span class="badge bg-light black"><?php print $mount_Employees; ?></span></label></a></li> -->
+        <li class="d-flex justify-content-between align-items-center"><a href="marks.php"><i class="icon list"></i><label for="">Marks</label></a></li>
         <li class="d-flex justify-content-between align-items-center"><a href="py.php"><i class="icon list"></i><label for="">Payment</label></a></li>
     </ul>
 
