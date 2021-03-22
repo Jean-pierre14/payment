819a8cd7663e29d87b594c8763c50db837143506
User <chiruzabisimwa@gmail.com>
2021-03-22 12:28:13 +0200

    marks page design

---
+++ includes/mark-data.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+    <h2 class="bg-success p-2 text-center text-white border-2">Marksheet</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <form action="">
+                <input type="search" name="search" id="search_mark" placeholder="Searching..." class="form-control">
+            </form>
+            list...
+        </div>
+        <div class="col-md-8"></div>
+    </div>
+</div>
