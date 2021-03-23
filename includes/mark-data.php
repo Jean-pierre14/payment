285b14813be44a83c9c6b1d2c795788a5152f7ac
User <chiruzabisimwa@gmail.com>
2021-03-23 09:41:12 +0200

    Instruction and get student

--- includes/mark-data.php
+++ includes/mark-data.php
@@ -1,17 +1,25 @@
 <div class="container-fluid">
     <h2 class="bg-success p-2 text-center text-white border-2">Marksheet</h2>
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-3 skicky-top">
             <form action="">
                 <input type="search" name="search" id="search_mark" placeholder="Searching..." class="form-control">
             </form>
-            list...
-            <div class="result" id="allStudents">
+            <div class="result mt-2" id="allStudents">
                 <!-- Ajax datas -->
             </div>
         </div>
         <div class="col-md-9">
-            <img src="undraw_Web_search_re_efla.png" alt="Instructions" class="img-fluid">
+            <?php if(isset($_GET['student'])):?>
+                <button onclick="window.history.back()" class="btn btn-sm btn-success">Back</button>
+                <input type="text" id="getStudent" value="<?= $_GET['student'];?>" class="form-control">
+                <div id="resultGetstudent">
+                    <!--Ajax data  -->
+                </div>
+            <?php else:?>
+                <h3 class="bg-success p-2 text-center text-white">INSTRUCTIONS</h3>
+                <img src="./images/undraw_Web_search_re_efla.png" alt="Instructions" class="img-fluid">
+            <?php endif;?>
         </div>
     </div>
 </div>
