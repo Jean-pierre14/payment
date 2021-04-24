dc7feb07571fbdae9729d243f344a5f92cd9de19
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-04-24 11:18:51 +0200

    Chartjs.io dynamic datas

---
+++ configuration/chart.js.php
@@ -0,0 +1,10 @@
+<?php
+include './db.con.php';
+
+if(isset($_POST['action'])){
+
+    if($_POST['action'] == 'StudentsCount'){
+        print 34;
+    }
+
+}
--- index.php
+++ index.php
@@ -9,18 +9,38 @@
 </div>
 
 <script src="./js/dist/Chart.bundle.min.js"></script>
+<script src="./js/jquery-3.4.0.min.js"></script>
+<script>
+    $(document).ready(function(){
+        Students();
+    })
+</script>
 
 <script>
     Chart.defaults.global.title.display = true;
-    Chart.defaults.global.title.text = 'Payment System';
+    Chart.defaults.global.title.text = 'System IO';
     Chart.defaults.globals.elements.point.radius = 10;
 </script>
 <script>
+    const resStudents = ''
+    function Students(){
+        
+        let action = 'StudentsCount'
+        $.ajax({
+            url: './configuration/chart.js.php',
+            method: 'POST',
+            data: {action},
+            success: function(data){
+                resStudents = data
+                alert("Data " + data)
+                alert(resStudents)
+            }
+        })
+    }
     let ctx = document.getElementById('myChart').getContext('2d');
     let chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-
     // The data for our dataset
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'May'],
@@ -29,7 +49,7 @@
             backgroundColor: 'rgb(255, 99, 132, 0.6)',
             borderColor: 'rgb(255, 99, 132, 0.6)',
             borderWidth: 1,
-            data: [0, 11, 3, 19, 21, 30, 45, 22, 35]
+            data: [2, 11, 3, 19, 21, 30, 45, 22, 35]
         },{
             label: 'Students',
             backgroundColor: 'rgb(20, 100, 122, 0.6)',
@@ -43,7 +63,7 @@
     // Configuration options go here
     options: {
         title: {
-            text: "Evolution of Data"
+            text: "System io"
         },
         resposinve: true,
         elements: {
