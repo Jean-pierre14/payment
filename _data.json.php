8215ffa2f971d763e880cc2e40c43859517976f8
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-05-08 14:21:11 +0200

    Fetch method fail to get data came from php json format

--- _data.json.php
+++ _data.json.php
@@ -34,3 +34,34 @@ if (isset($_POST['action'])) {
         print json_encode($output);
     }
 }
+
+// Get method
+
+if (isset($_GET['action'])) {
+
+    if ($_GET['action'] == 'student') {
+        $sql = sprintf("SELECT * FROM student ORDER BY id_student DESC");
+        $query = mysqli_query($con, $sql);
+        if (@mysqli_num_rows($query) > 0) {
+            while ($row = mysqli_fetch_array($query)) {
+                print json_encode($row);
+            }
+        } else {
+            print json_encode("there is no data");
+        }
+    }
+
+    if ($_GET['action'] == 'getStudent') {
+        $Id = mysqli_real_escape_string($con, trim(htmlentities($_POST['id'])));
+
+        $sql = mysqli_query($con, sprintf("SELECT * FROM student WHERE student_id"));
+        if (@mysqli_num_rows($sql) > 0) {
+            while ($row = mysqli_fetch_array($sql)) {
+                $output[] = $row;
+            }
+        } else {
+            $output = 'there no data';
+        }
+        print json_encode($output);
+    }
+}
