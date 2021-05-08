20358d537e83d40d95a1c33097d16c6872e89618
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-05-08 13:53:28 +0200

    new changes and json format using php

---
+++ _data.json.php
@@ -0,0 +1,36 @@
+<?php
+
+include './configuration/db.con.php';
+
+$action = '';
+$output = '';
+$error = '';
+
+if (isset($_POST['action'])) {
+
+    if ($_POST['action'] == 'student') {
+        $sql = sprintf("SELECT * FROM student ORDER BY id_student DESC");
+        $query = mysqli_query($con, $sql);
+        if (@mysqli_num_rows($query) > 0) {
+            while ($row = mysqli_fetch_array($query)) {
+                print json_encode($row);
+            }
+        } else {
+            print json_encode("there is no data");
+        }
+    }
+
+    if ($_POST['action'] == 'getStudent') {
+        $Id = mysqli_real_escape_string($con, trim(htmlentities($_POST['id'])));
+
+        $sql = mysqli_query($con, sprintf("SELECT * FROM student WHERE student_id"));
+        if (@mysqli_num_rows($sql) > 0) {
+            while ($row = mysqli_fetch_array($sql)) {
+                $output[] = $row;
+            }
+        } else {
+            $output = 'there no data';
+        }
+        print json_encode($output);
+    }
+}
