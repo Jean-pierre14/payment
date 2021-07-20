e281921ab6bc6c772cfc51a451d7bd544970b15d
User <chiruzabisimwa@gmail.com>
2021-07-20 20:41:54 +0200

    You can add the code at the without the semicolon

--- php/index.php
+++ php/index.php
@@ -2,3 +2,9 @@
     // header("Content-Type: text/plain"); #This will display data into the text/plain 
     // print "Hell world";
     // to display a json form you have to config the header like so
+
+    // header("Content-Type: application/json");
+    // $res = ['response' => 'json format'];
+    // print json_encode($res);
+    print "Display";
+    print "without semicolon if you close your statement <?php?>"?>
