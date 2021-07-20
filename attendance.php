8809fbf57d8b5184303e497a3e4e4e4f912b2bf9
User <chiruzabisimwa@gmail.com>
2021-07-20 17:01:30 +0200

    Test

--- attendance.php
+++ attendance.php
@@ -1,5 +1,33 @@
 <?php
 
-$today = date('Y-m-d');
-$l = $today - 1;
-print $today .' -- '.$l;
+$t = 'code';
+$test = filter_var($t, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+
+print "Output ". $test;
+
+function f(&$s){
+    $st = "world";
+}
+$s1 = "Hello";
+echo $s1;
+f($s1);
+echo $s1;
+
+echo "___________________________________________";
+$people = array(
+    array("Steven", 22, 18),
+    array("Bob", 15,13)
+);
+for($row = 0; $row < 2; $row++){
+    echo "<ul>";
+    for($col = 0; $col < 3; $col++){
+        echo "<li>". $people[$row][$col]. "<li>";
+    }
+    echo "</ul>";
+}
+$a = "";
+if($a){
+    print "All";
+}else{
+    print "some";
+}
