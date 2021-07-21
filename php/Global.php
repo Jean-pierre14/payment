8d54bfd5f72a6ccb462e41b708525f29d884a524
User <chiruzabisimwa@gmail.com>
2021-07-21 08:20:12 +0200

    Global var

--- php/Global.php
+++ php/Global.php
@@ -6,6 +6,13 @@
         var_dump($GLOBALS['var']);
     }
     test();
-    var_dump($varIn);
     var_dump($var);
+    var_dump($varIn);
+
+    function test2(){
+        global $var; # to create a global variable you need to declare it first
+        $var = "Hello mind";
+    }
+    test2();
+    print $var;
 ?>
