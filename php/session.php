0b10c093c963d787616a14a4e4a86fbd0855b8a2
User <chiruzabisimwa@gmail.com>
2021-07-23 07:41:44 +0200

    Session

---
+++ php/session.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+
+    $username = 'Grace';
+    $auth = 1;
+    $_SESSION['username'] = $username;
+
+    if(isset($_SESSION['username'])):
+        header(`Location: index.php'. md5($auth).'?'.$username.'`)
+    endif;
