e50409ff656f6fd13440be0d74b5d1d0358a37ed
User <chiruzabisimwa@gmail.com>
2021-07-23 18:24:21 +0200

    PDO connection and getting data from DB

--- configuration/_pdo.php
+++ configuration/_pdo.php
@@ -5,5 +5,20 @@
 
 // $postgres = "pgsql:host=127.0.0.1;port=5432;dbname=dbname";
 
+$dsn = "mysql:host=localhost;dbname=crudajax;charset=utf8";
+$username = 'root';
+$password = '';
 
+// To setup DB connection
+$conn = new PDO($dsn,$username,$password) OR die("DB not found");
+// Setup the PDO if the invalid query is provide
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Let us make a query
+$query = "SELECT * FROM users";
+$statement = $conn->query($query);
+
+header('Content-Type: application/json');
+while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+    print json_encode($row);
+}
 
