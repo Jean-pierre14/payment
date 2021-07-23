8908dedf3666dedd0b31fd09f7c812f4a9b70c43
User <chiruzabisimwa@gmail.com>
2021-07-23 17:45:17 +0200

    Front-end of the PDO

---
+++ php/codes.php
@@ -0,0 +1,30 @@
+<?php
+    // header("Content-Type: text/plain"); #This will display data into the text/plain 
+    // print "Hell world";
+    // to display a json form you have to config the header like so
+
+    // header("Content-Type: application/json");
+    // $res = ['response' => 'json format'];
+    // print json_encode($res);
+    print "Display\n";
+    print "without semicolon if you close your statement <?php?>\n";
+    
+    ?>
+
+<?php
+
+function myFunction($a, $b){
+    return $a + $b;
+}
+$run = 'myFunction';
+print $run(3, 4);
+
+class myClass{
+    public function __construct(){
+        $functionName = "Grace";
+        $this->$functionName('Hello wolrd');
+    }
+    private function Grace($string){
+        print $string;
+    }
+}
--- php/index.php
+++ php/index.php
@@ -1,30 +1,29 @@
-<?php
-    // header("Content-Type: text/plain"); #This will display data into the text/plain 
-    // print "Hell world";
-    // to display a json form you have to config the header like so
+<!DOCTYPE html>
+<html lang="en">
 
-    // header("Content-Type: application/json");
-    // $res = ['response' => 'json format'];
-    // print json_encode($res);
-    print "Display\n";
-    print "without semicolon if you close your statement <?php?>\n";
-    
-    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP code</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
 
-<?php
+<body>
+    <div class="container">
+        <div class="form">
+            <h3>PDO code PHP</h3>
+            <form action="./condes.php" method="get">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="Username">
+                <label for="fullname">Fullname</label>
+                <input type="text" name="fullname" id="fullname" placeholder="Fullname">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Email@grace.cd">
+                <button class="button" type="submit">Register</button>
+            </form>
+        </div>
+    </div>
+</body>
 
-function myFunction($a, $b){
-    return $a + $b;
-}
-$run = 'myFunction';
-print $run(3, 4);
-
-class myClass{
-    public function __construct(){
-        $functionName = "Grace";
-        $this->$functionName('Hello wolrd');
-    }
-    private function Grace($string){
-        print $string;
-    }
-}
+</html>
