f62a761965e1eaee1eeafac1205c96f9e7f8f69d
User <chiruzabisimwa@gmail.com>
2021-07-23 07:35:23 +0200

    cookies

--- php/cookies.php
+++ php/cookies.php
@@ -1,7 +1,7 @@
 <?php
 
-    // setcookie('var', 'value', time() + 3600);
-    // echo $_COOKIE['var'];
+    setcookie('var1', 'value', time() + 3600);
+    echo $_COOKIE['var1'];
     // to destroy a cookie to do like so
     setcookie('var', '', time() -1);
     var_dump($_COOKIE['var']);
