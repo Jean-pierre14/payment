bcec5842b08830fbd3123c8aff3fa8904b82e0be
User <chiruzabisimwa@gmail.com>
2021-08-21 11:39:37 +0200

    init

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bisimwa</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/progress.css">
 </head>
+
 <body>
-    
+    <nav>
+        <a href="#" class="logo">Mr BISIMWA</a>
+        <ul class="nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Rgister</a></li>
+            <li><a href="#">Attendance</a></li>
+        </ul>
+    </nav>
+
 </body>
+
 </html>
