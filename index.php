06dd34f600e8a5bb850b9e739c3d8d8b769c786b
User <chiruzabisimwa@gmail.com>
2021-08-21 11:47:45 +0200

    New UI

--- index.php
+++ index.php
@@ -11,15 +11,90 @@
 </head>
 
 <body>
-    <nav>
-        <a href="#" class="logo">Mr BISIMWA</a>
-        <ul class="nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Rgister</a></li>
-            <li><a href="#">Attendance</a></li>
-        </ul>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="100">
+        <div class="container">
+            <div class="navbar-translate">
+                <a class="navbar-brand" href="https://demos.creative-tim.com/blk-design-system/index.html" rel="tooltip"
+                    title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
+                    <span>BLK•</span> Design System
+                </a>
+                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navigation" aria-controls="navigation-index" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                <div class="navbar-collapse-header">
+                    <div class="row">
+                        <div class="col-6 collapse-brand">
+                            <a>
+                                BLK•
+                            </a>
+                        </div>
+                        <div class="col-6 collapse-close text-right">
+                            <button type="button" class="navbar-toggler" data-toggle="collapse"
+                                data-target="#navigation" aria-controls="navigation-index" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                                <i class="tim-icons icon-simple-remove"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <ul class="navbar-nav">
+                    <li class="nav-item p-0">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom"
+                            href="https://twitter.com/CreativeTim" target="_blank">
+                            <i class="fab fa-twitter"></i>
+                            <p class="d-lg-none d-xl-none">Twitter</p>
+                        </a>
+                    </li>
+                    <li class="nav-item p-0">
+                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom"
+                            href="https://www.facebook.com/CreativeTim" target="_blank">
+                            <i class="fab fa-facebook-square"></i>
+                            <p class="d-lg-none d-xl-none">Facebook</p>
+                        </a>
+                    </li>
+                    <li class="nav-item p-0">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom"
+                            href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+                            <i class="fab fa-instagram"></i>
+                            <p class="d-lg-none d-xl-none">Instagram</p>
+                        </a>
+                    </li>
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                            <i class="fa fa-cogs d-lg-none d-xl-none"></i> Getting started
+                        </a>
+                        <div class="dropdown-menu dropdown-with-icons">
+                            <a href="https://demos.creative-tim.com/blk-design-system/docs/1.0/getting-started/overview.html"
+                                class="dropdown-item">
+                                <i class="tim-icons icon-paper"></i> Documentation
+                            </a>
+                            <a href="examples/register-page.html" class="dropdown-item">
+                                <i class="tim-icons icon-bullet-list-67"></i>Register Page
+                            </a>
+                            <a href="examples/landing-page.html" class="dropdown-item">
+                                <i class="tim-icons icon-image-02"></i>Landing Page
+                            </a>
+                            <a href="examples/profile-page.html" class="dropdown-item">
+                                <i class="tim-icons icon-single-02"></i>Profile Page
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-default d-none d-lg-block" href="javascript:void(0)"
+                            onclick="scrollToDownload()">
+                            <i class="tim-icons icon-cloud-download-93"></i> Download
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
-
 </body>
 
 </html>
