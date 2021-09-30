4915fec0465818b4311bf092336ff4da545d8cfd
User <chiruzabisimwa@gmail.com>
2021-09-30 13:24:07 +0200

    Admin Option or settings UI

--- includes/sideBar.php
+++ includes/sideBar.php
@@ -17,7 +17,18 @@
         <li class="d-flex justify-content-between align-items-center"><a href="lecturer.php"><i class="icon users"></i><label for="">Lecturers<span class="badge bg-light black"><?php print $mount_Lecturer; ?></span></label></a></li>
         <!-- <li class="d-flex justify-content-between align-items-center"><a href=""><i class="icon trash"></i><label for="">Employees<span class="badge bg-light black"><?php print $mount_Employees; ?></span></label></a></li> -->
         <li class="d-flex justify-content-between align-items-center"><a href="marks.php"><i class="icon list"></i><label for="">Marks</label></a></li>
-        <li class="d-flex justify-content-between align-items-center"><a href="py.php"><i class="icon list"></i><label for="">Payment</label></a></li>
+        <li class="d-flex justify-content-between align-items-center">
+            <a href="py.php">
+                <i class="icon list"></i>
+                <label for="">Payment</label>
+            </a>
+        </li>
+        <li class="d-flex justify-content-between align-items-center">
+            <a href="adminOption.php">
+                <i class="icon settings"></i>
+                <label for="">Settings</label>
+            </a>
+        </li>
     </ul>
 
     <!-- <div class="mt-5 p-1">
