226dd940c843bc530910f45913ccb94bb51068a2
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-12-06 10:48:11 +0100

    init part two new version

--- studentAdd.php
+++ studentAdd.php
@@ -1,4 +1,3 @@
-
 <?php $get_std = false;
 include("./includes/nav.php");
 
@@ -22,45 +21,50 @@ if (!isset($_SESSION['username'])) {
                         <?php include "./error.php";?>
                         <div class="form-group">
                             <label for="username">Username</label>
-                            <input type="text" name="username" value="<?= $stud_username; ?>" id="username" placeholder="Username" class="form-control">
+                            <input type="text" name="username" value="<?= $stud_username; ?>" id="username"
+                                placeholder="Username" class="form-control">
                         </div>
 
                         <div class="form-group">
                             <label for="sname">Second name</label>
-                            <input type="text" name="sname" value="<?= $stud_sname; ?>" id="sname" placeholder="Second name" class="form-control">
+                            <input type="text" name="sname" value="<?= $stud_sname; ?>" id="sname"
+                                placeholder="Second name" class="form-control">
                         </div>
 
                         <div class="form-row">
-                                <div class="form-group col-md-4">
-                                        <label for="class">Class </label>
+                            <div class="form-group col-md-4">
+                                <label for="class">Class </label>
 
-                                        <div class="ui" id="classess">
-                                            <select name="class" value="<?= $stud_class; ?>" id="class" class="form-control" require>
-                                                <option value="">-- select --</option>
-                                                <option value="YEAR 1">Year 1</option>
-                                                <option value="YEAR 2">Year 2</option>
-                                                <option value="YEAR 3">Year 3</option>
-                                            </select>
-                                        </div>
-                                    </div>
+                                <div class="ui" id="classess">
+                                    <select name="class" value="<?= $stud_class; ?>" id="class" class="form-control"
+                                        require>
+                                        <option value="">-- select --</option>
+                                        <option value="YEAR 1">Year 1</option>
+                                        <option value="YEAR 2">Year 2</option>
+                                        <option value="YEAR 3">Year 3</option>
+                                    </select>
+                                </div>
+                            </div>
 
-                                    <div class="form-group col-md-8">
-                                        <label for="depart">Department</label>
-                                        <select name="depart" value="<?= $stud_depart; ?>" id="depart" class="form-control" require>
-                                            <option value="">-- select --</option>
-                                            <option value="computer science">Computer Science</option>
-                                            <option value="Law">Law</option>
-                                            <option value="Finance">Finance</option>
-                                            <option value="Economy">Economy</option>
-                                            <option value="Accounting">Accounting</option>
-                                        </select>
-                                    </div>
+                            <div class="form-group col-md-8">
+                                <label for="depart">Department</label>
+                                <select name="depart" value="<?= $stud_depart; ?>" id="depart" class="form-control"
+                                    require>
+                                    <option value="">-- select --</option>
+                                    <option value="computer science">Computer Science</option>
+                                    <option value="Law">Law</option>
+                                    <option value="Finance">Finance</option>
+                                    <option value="Economy">Economy</option>
+                                    <option value="Accounting">Accounting</option>
+                                </select>
+                            </div>
                         </div>
                         <div class="form-row">
 
                             <div class="form-group col-md-3">
                                 <label for="sex">Sex</label>
-                                <select name="sex" value="<?php print $stud_sex; ?>" id="sex" class="form-control" require>
+                                <select name="sex" value="<?php print $stud_sex; ?>" id="sex" class="form-control"
+                                    require>
                                     <option value="">-- select --</option>
                                     <option value="Male">Male</option>
                                     <option value="Female">Female</option>
@@ -68,21 +72,25 @@ if (!isset($_SESSION['username'])) {
                             </div>
 
                             <div class="form-group col-md-9">
-                                
+
                                 <?php if($email_edit == true):?>
-                                    <label for="email">Email</label>
-                                    <input type="email" value="<?php print $stud_em; ?>" name="email" id="email" placeholder="Email" class="form-control" readonly>
+                                <label for="email">Email</label>
+                                <input type="email" value="<?php print $stud_em; ?>" name="email" id="email"
+                                    placeholder="Email" class="form-control" readonly>
                                 <?php else: ?>
-                                    <label for="email">Email</label>
-                                    <input type="email" value="<?php print $stud_em; ?>" name="email" id="email" placeholder="Email" class="form-control"> 
+                                <label for="email">Email</label>
+                                <input type="email" value="<?php print $stud_em; ?>" name="email" id="email"
+                                    placeholder="Email" class="form-control">
                                 <?php endif;?>
                             </div>
                         </div>
-                        <div class="form-group">   
+                        <div class="form-group">
                             <?php if($update == true): ?>
-                                <button type="submit" name="student_update" class="icon ui labeled button green"><i class="icon record"></i>UPDATE</button>
+                            <button type="submit" name="student_update" class="icon ui labeled button green"><i
+                                    class="icon record"></i>UPDATE</button>
                             <?php else:?>
-                                <button type="submit" id="register_student" name="register" class="icon ui labeled button blue"><i class="icon save"></i>Register</button>
+                            <button type="submit" id="register_student" name="register"
+                                class="icon ui labeled button blue"><i class="icon save"></i>Register</button>
                             <?php endif;?>
                         </div>
                     </form>
@@ -95,7 +103,7 @@ if (!isset($_SESSION['username'])) {
     </div>
 </div>
 <script>
-    function functionBack(){
-        window:location.href = 'index.php'
-    }
+function functionBack() {
+
+}
 </script>
