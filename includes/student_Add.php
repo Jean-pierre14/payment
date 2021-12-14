d632f21e199cd25b047d4f9a1cc557cd63f43836
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-12-14 13:01:34 +0100

    Data using PHP

--- includes/student_Add.php
+++ includes/student_Add.php
@@ -3,70 +3,73 @@
     <div class="row justify-content-center">
         <div class="col-md-11 p-0 pr-2">
             <a href="studentAdd.php" class="mt-2 btn btn-sm btn-success">Add new student</a>
-            <div class="card card-body mt-2">
-                <form action="" method="post">
-                    <input type="search" placeholder="Search..." class="my-2 form-control">
-                </form>
-                <div id="result">
-                    <!-- API json -->
-                </div>
-                <table class="table m-0 table-responsive-sm table-sm table-hover table-hover">
-                    <thead>
-                        <tr>
-                            <th> Name </th>
-                            <th> Second name </th>
-                            <th> Email </th>
-                            <th> Class </th>
-                            <th> Department </th>
-                            <th> Actions </th>
-                        </tr>
-                    </thead>
-                </table>
-                <div class="table-responsive table-responsive-sm table-size m-0 table-sm table-hover table-hover">
-                    <table class="table table-striped">
+            <div class="container-fluid p-0">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-body mt-2">
+                            <form action="" method="post">
+                                <input type="search" placeholder="Search..." class="my-2 form-control">
+                            </form>
+                            <div class="list-group">
+                                <?php while($data = mysqli_fetch_array($run_student)): ?>
 
-                        <tbody>
+                                <a class="list-group-item"
+                                    href="student.php?getStudent=<?php echo $data['id_student'];?>">
+                                    <?php echo $data['username'];?>
+                                </a>
 
-                            <?php while($data = mysqli_fetch_array($run_student)): ?>
-                            <tr>
-                                <td>
-                                    <?php print $data['username'];?>
-                                </td>
-                                <td>
-                                    <?php print $data['sname'];?>
-                                </td>
-                                <td>
-                                    <?php print $data['email'];?>
-                                </td>
-                                <td>
-                                    <?php print $data['class'];?>
-                                </td>
-                                <td>
-                                    <?php print $data['depart'];?>
-                                </td>
-                                <td>
-                                    <div class="btn-group">
-                                        <a title="edit <?php print $data['sname'] ;?>" class="btn btn-sm btn-primary"
-                                            href="student.php?edt=<?php echo $data['id_student']; ?>"><i
-                                                class="icon edit"></i></a>
-                                        <a title="delete <?php print $data['sname'] ;?>" onclick="CheckDelete()"
-                                            class="btn btn-sm btn-danger text-light"
-                                            href="student.php?del=<?php print $data['id_student']; ?>"><i
-                                                class="icon trash"></i></a>
-                                    </div>
-                                </td>
-                            </tr>
-                            <?php endwhile; ?>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="bg-success white p-2">
-                    <tr>
-                        <td style="width: 80%;">Total of student </td>
-                        <td><span class="badge badge-info">
-                                <?php print $mount_Student;?>
-                            </span></td>
-                    </tr>
+                                <?php endwhile;?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <?php if(isset($_GET['getStudent'])):?>
+                        <?php $id = $_GET['getStudent'];
+                                $sql = mysqli_query($con, "SELECT * FROM student WHERE id_student = ${id}");
+                                $out = '';
+                                $out .= '<a href="student.php?edit='.$row['id_student'].'" class="btn btn-sm btn-warning mt-2">Edit</a>'
+                                if(mysqli_num_rows($sql) > 0){
+                                    $out .= '<div class="list-group">';
+                                    while($row = mysqli_fetch_array($sql)){
+                                        $out .= '
+                                        <p class="list-group-item d-flex justify-content-between align-items-center">
+                                            <span>Username:</span>
+                                            <span>'.$row['username'].'</span>
+                                        </p>
+
+                                        <p class="list-group-item d-flex justify-content-between align-items-center">
+                                            <span>Fullname:</span>
+                                            <span>'.$row['sname'].'</span>
+                                        </p>
+
+                                        <p class="list-group-item d-flex justify-content-between align-items-center">
+                                            <span>Class:</span>
+                                            <span>'.$row['class'].'</span>
+                                        </p>
+
+                                        <p class="list-group-item d-flex justify-content-between align-items-center">
+                                            <span>Sexe:</span>
+                                            <span>'.$row['sex'].'</span>
+                                        </p>
+
+                                        <p class="list-group-item d-flex justify-content-between align-items-center">
+                                            <span>Enregistre:</span>
+                                            <span>'.$row['created_at'].'</span>
+                                        </p>
+                                        ';
+                                    }
+                                    $out .= '</div>';
+                                }else{
+                                    $out = '<p  class="alert alert-danger">:( Mal jouer</p>';
+                                }
+                                print $out;
+                            ?>
+                        <?php else:?>
+                        <div class="card card-body mt-2">
+                            <h2>Student platform</h2>
+                        </div>
+                        <?php endif;?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -75,30 +78,31 @@
 
 <script src="./js/jquery-3.4.0.min.js"></script>
 <script>
-    $(document).ready(function(){
-        Select()
-    })
+$(document).ready(function() {
+    Select()
+})
 
-    function Select(){
-        let action = 'select_student'
-        $.ajax({
-            url: './configuration/action.php',
-            method: 'POST',
-            dataType: 'JSON',
-            data: {action},
-            success: function(data){
-                if(data !== 'error'){
-                    alert('This no internet')
-                }else{
-                    $('#result').innerHTML = data
-                }
+function Select() {
+    let action = 'select_student'
+    $.ajax({
+        url: './configuration/action.php',
+        method: 'POST',
+        dataType: 'JSON',
+        data: {
+            action
+        },
+        success: function(data) {
+            if (data !== 'error') {
+                alert('This no internet')
+            } else {
+                $('#result').innerHTML = data
             }
-        })
-    }
-    
+        }
+    })
+}
 </script>
 <script>
-    function CheckDelete() {
-        confirm('Do you want to delete?')
-    }
+function CheckDelete() {
+    confirm('Do you want to delete?')
+}
 </script>
