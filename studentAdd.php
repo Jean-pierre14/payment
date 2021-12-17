e25480b82b8d0b3fe99ab4523d648d51d48c699d
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-12-17 06:30:45 +0100

    Student Edit

--- studentAdd.php
+++ studentAdd.php
@@ -20,36 +20,39 @@ if (!isset($_SESSION['username'])) {
                     <form autocomplete="off" action="" method="post">
                         <?php include "./error.php";?>
                         <div class="form-group">
-                            <label for="username">Username</label>
+                            <label for="username">Prenom</label>
                             <input type="text" name="username" value="<?= $stud_username; ?>" id="username"
                                 placeholder="Username" class="form-control">
                         </div>
 
                         <div class="form-group">
-                            <label for="sname">Second name</label>
-                            <input type="text" name="sname" value="<?= $stud_sname; ?>" id="sname"
-                                placeholder="Second name" class="form-control">
+                            <label for="sname">Nom</label>
+                            <input type="text" name="sname" value="<?= $stud_sname; ?>" id="sname" placeholder="Nom"
+                                class="form-control">
                         </div>
 
                         <div class="form-row">
                             <div class="form-group col-md-4">
-                                <label for="class">Class </label>
+                                <label for="class">Classe </label>
 
                                 <div class="ui" id="classess">
                                     <select name="class" value="<?= $stud_class; ?>" id="class" class="form-control"
                                         require>
                                         <option value="">-- select --</option>
-                                        <option value="YEAR 1">Year 1</option>
-                                        <option value="YEAR 2">Year 2</option>
-                                        <option value="YEAR 3">Year 3</option>
+                                        <option value="P1">P1</option>
+                                        <option value="P2">P2</option>
+                                        <option value="P3">P3</option>
+                                        <option value="P4">P4</option>
+                                        <option value="P5">P5</option>
+                                        <option value="P6">P6</option>
                                     </select>
                                 </div>
                             </div>
 
                             <div class="form-group col-md-8">
-                                <label for="depart">Department</label>
-                                <select name="depart" value="<?= $stud_depart; ?>" id="depart" class="form-control"
-                                    require>
+                                <label for="depart">Annee scolaire</label>
+                                <select name="AnneScolaire" value="<?= $stud_depart; ?>" id="AnneeScolaire"
+                                    class="form-control" require>
                                     <option value="">-- select --</option>
                                     <option value="computer science">Computer Science</option>
                                     <option value="Law">Law</option>
@@ -62,23 +65,23 @@ if (!isset($_SESSION['username'])) {
                         <div class="form-row">
 
                             <div class="form-group col-md-3">
-                                <label for="sex">Sex</label>
+                                <label for="sex">Sexe</label>
                                 <select name="sex" value="<?php print $stud_sex; ?>" id="sex" class="form-control"
                                     require>
                                     <option value="">-- select --</option>
-                                    <option value="Male">Male</option>
-                                    <option value="Female">Female</option>
+                                    <option value="Male">Masculin</option>
+                                    <option value="Female">Feminin</option>
                                 </select>
                             </div>
 
                             <div class="form-group col-md-9">
 
                                 <?php if($email_edit == true):?>
-                                <label for="email">Email</label>
+                                <label for="email">E-mail des parents</label>
                                 <input type="email" value="<?php print $stud_em; ?>" name="email" id="email"
                                     placeholder="Email" class="form-control" readonly>
                                 <?php else: ?>
-                                <label for="email">Email</label>
+                                <label for="email">E-mail des parents</label>
                                 <input type="email" value="<?php print $stud_em; ?>" name="email" id="email"
                                     placeholder="Email" class="form-control">
                                 <?php endif;?>
