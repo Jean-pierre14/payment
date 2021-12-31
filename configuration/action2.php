beb1f32370da5f023fa30d9ac40db187ec64f0de
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-12-31 07:31:38 +0100

    Fix

--- configuration/action2.php
+++ configuration/action2.php
@@ -9,9 +9,10 @@
     if(@mysqli_num_rows($sql) > 0){
         $output .= '<div class="list-group list-group-flush">';
         while($row = mysqli_fetch_array($sql)){
-            $output .= '<a href="student.php?getStudent='.$row['id_student'].'" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-            <span>'.$row['username'].'</span>
-            <span class="badge badge-success">'.$row['class'].'</span>
+            $output .= '
+            <a href="student.php?getStudent='.$row['id_student'].'" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                <span>'.$row['username'].'</span>
+                <span class="badge badge-success">'.$row['class'].'</span>
             </a>';
         }
         $output .= '</div>';
