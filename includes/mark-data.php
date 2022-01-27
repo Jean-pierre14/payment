67f9265f3c0ec516f67f568fa48c8a4ad27f0e28
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-01-27 12:35:33 +0100

    Display data using a function

--- includes/mark-data.php
+++ includes/mark-data.php
@@ -3,6 +3,26 @@
     <div class="row">
         <div class="col-md-3 skicky-top">
             <h3>List</h3>
+            <?php
+                $output = '';
+                function Fetch(){
+                    $output = 'Loading...';
+
+                    global $con;
+                    $sql = mysqli_query($con, "SELECT * FROM student");
+
+                    if(@mysqli_num_rows($sql) > 0){
+                        $output = '';
+                        while($row = mysqli_fetch_array($sql)){
+                            $output .= '<p>'.$row['username'].'</p>';
+                        }
+                    }else{
+                        $output = '<p>There is no data</p>';
+                    }
+                    return $output;
+                }
+                echo Fetch();
+            ?>
             <!-- Ajax datas -->
         </div>
         <div class="col-md-9">
