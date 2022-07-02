dd3e51e3e34c0fbfa46bdf7f317a654771b2fb3b
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-07-02 15:26:54 +0200

    UI of the marksheet

--- includes/mark-data.php
+++ includes/mark-data.php
@@ -1,6 +1,46 @@
 <div class="container-fluid">
     <h2 class="bg-success p-2 text-center text-white border-2">Cotation</h2>
     <div class="elements">
+        <div class="row py-3">
+            <div class="col-md-4">
+                <form action="" method="post">
+                    <input type="search" name="search" id="search" placeholder="Recherche..." class="form-control">
+                </form>
+
+                <div id="elements">
+                    <ul class="pt-2 list-group">
+                        <li class="list-group-item list-group-success list-group-item-action d-flex justify-content-between align-items-centers">
+                            <span>Item</span>
+                            <span class="badge badge-success">2021-2022</span>
+                        </li>
+                        <li class="list-group-item list-group-success list-group-item-action">item</li>
+                    </ul>
+                </div>
+                
+            </div>
+            <div class="col-md-8">
+                <div class="card card-body">
+                    <h2>Manuel d'utilisation</h2>
+                    <p>
+                        Bienvenue dans cette interface faite pour travailler la cotation.
+                        Dans celle-ci vous pouvez tout faire comme:
+                        
+                    </p>
+                    <div class=" p-3 content-center">
+                        <ol>
+                            <li>Ajouter les notes</li>
+                            <li>Modifier les notes</li>
+                            <li>Mettres a* jour les notes</li>
+                            <li>Effacer les notes</li>
+                        </ol>
+                    </div>
+                    <h2>Pour l'option recherche il faut...</h2>
+                    <p>
+                        d'abord savoir ce que vous desirez trouver. Ensuite, allez tapez dans la barre de cherche le nom, la classe, l'age, l'annee, ou meme un numero de telephone.
+                    </p>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-3">
                 <select name="classes" id="classes" class="form-control">
